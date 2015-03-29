319f4996301b4eb6993702185457e1c4aedb780e
Gyengus <gyengus@gyengus.hu>
2015-03-29 12:45:56 +0200

    More modification
    News reader moved to module lib/news.js
    Setting up logger to create access.log in Apache combined format

--- app.js
+++ app.js
@@ -4,44 +4,47 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var routes = require('./routes/index');
 
 var app = express();
 
+app.LOGDIR = 'logs';
+
 // Read version from package.json
 app.APP_VERSION = "N/A";
-var fs = require('fs');
-fs.readFile('package.json', 'utf8', function (err, data) {
+fs.readFile(__dirname + '/package.json', 'utf8', function (err, data) {
 	if (err) console.log(err);
 	var package_data = JSON.parse(data);
 	app.APP_VERSION = package_data.version;
 	console.log('Application version: ' + app.APP_VERSION);
 });
 
+console.log('Dirname: ' + __dirname);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'web/views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.enable('trust proxy');
-app.use(logger('dev')); // dev http://www.hacksparrow.com/express-js-logging-access-and-errors.html
+var accesslogStream = fs.createWriteStream(__dirname + '/' + app.LOGDIR + '/access.log', {flags: 'a'});
+app.use(logger('combined', {stream: accesslogStream}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/files', express.static('downloads'));
+app.use(__dirname + '/files', express.static('downloads'));
 
 // a middleware with no mount path, gets executed for every request to the router
 app.use(function (req, res, next) {
 	req.APP_VERSION = app.APP_VERSION;
-	var most = new Date(); // Ezt a dátumformázást be kellene rakni a Date.prototype-ba
+	/*var most = new Date(); // Ezt a dátumformázást be kellene rakni a Date.prototype-ba
 	//console.log('Time:', most.getFullYear() + '-' + (most.getMonth() + 1) + '-' + most.getDate() + ' ' + most.getHours() + ':' + most.getMinutes() + ':' + most.getSeconds() + '.' + most.getMilliseconds());
-	fs.appendFile('logs/access.log', req.originalUrl + '\n' + JSON.stringify(req.headers) + '\n' + JSON.stringify(req.params) + '\n', function(err) {
+	fs.appendFile(app.LOGDIR + '/access.log', req.originalUrl + '\n' + JSON.stringify(req.headers) + '\n' + JSON.stringify(req.params) + '\n', function(err) {
 		if (err) console.log('Hiba az access.log írása közben: ' + err);
-	});
+	});*/
 	next();
 });
 
@@ -84,7 +87,7 @@ app.use(function(err, req, res, next) {
 	'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
 	].forEach(function(element, index, array) {
 		process.on(element, function() {
-			console.log('%s: Node server stopped.', Date(Date.now()), element);
+			console.log('%s: Node server stopped by %s signal.', Date(Date.now()), element);
 			process.exit();
 		});
 	});
--- routes/index.js
+++ routes/index.js
@@ -4,75 +4,13 @@ var router = express.Router();
 var fs = require('fs');
 var dns = require('dns');
 var crypto = require('crypto');
-var parseString = require('xml2js').parseString;
 
-var readnews = function(callback) {
-	//console.log('read news...');
-	fs.readFile('cache/news.json', function(err, data) {
-		if (err) {
-			// feed letöltése
-			var http_client = require('http');
-			http_client.get('http://gyengus.hu/feed/', function(res) {
-				var body = '';
-				res.on('data', function(chunk) {
-					body += chunk;
-				});
-				res.on('end', function() {
-					/*fs.writeFile('cache/news.xml', body, function(err) {
-						if (err) console.log(err);
-					});*/
-					parseString(body, function (err, result) {
-						var news = [];
-						//fs.writeFile('cache/news.json', JSON.stringify(result));
-						//console.log(result.rss.channel[0].item[0].title[0] + ' ' + result.rss.channel[0].item[0].link[0]);
-						result.rss.channel[0].item.forEach(function(item) {
-							//console.log(item.title[0] + ' ' + item.description[0]);
-							var obj = {};
-							obj.title = item.title[0];
-							obj.link = item.link[0];
-							obj.desc = item.description[0];
-							news.push(obj);
-						});
-						fs.writeFile('cache/news.json', JSON.stringify(news), function(err) {
-							if (err) console.log(err);
-							readnews(callback);
-						});
-					});
-				});
-			}).on('error', function(err) {
-				console.log(err.message);
-			});
-		} else {
-			fs.stat('cache/news.json', function(err, stats) {
-				if (err) console.log(err);
-				var d = new Date(stats.mtime).getTime();
-				var most = new Date().getTime();
-				if ((most - d) >= 300000) {
-					fs.unlink('cache/news.json', function() {
-						readnews(callback);
-					});
-				} else {
-					var news_data = JSON.parse(data);
-					var news_html = '';
-					var x = news_data.length;
-					if (x > 10) x = 10;
-					for (var i = 0; i < x; i++) {
-						news_html += '<div class="news"><a href="' + news_data[i].link + '">'
-								  + news_data[i].title + '</a><a href="#" class="expandbtn"><span class="icon icon-circle-down"></span></a>'
-								  + '<div class="news_desc">' + news_data[i].desc + '</div></div>';
-					}
-					//console.log('news_html: ' + news_html);
-					callback(news_html);
-				}
-			});
-		}
-	});
-};
-
+var news = require('../lib/news');
+console.log(news);
 
 // Index
 router.get('/', function(req, res, next) {
-	readnews(function(news) {
+	news.read(function(news) {
 		//console.log('news: ' + news);
 		var ip = req.connection.remoteAddress;
 		//console.log('Client IP: ' + ip);
